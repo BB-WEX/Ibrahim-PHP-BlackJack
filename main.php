8d1132f825f7978f536f8e8e17413fb80ceb8726
Ibrahimm08 <imaqsood360@outlook.com>
2025-10-09 11:07:11 +0100

    Added message of final hands and calculate winner

--- main.php
+++ main.php
@@ -11,7 +11,7 @@ $deck = new deck();
 $deck->shuffle_deck();
 
 // Create an instanc of bothe player and dealer
-$player = new Player("Player 1");
+$player = new Player("Player");
 $player = new dealer("Dealer");
 
 
@@ -41,5 +41,29 @@ while ($player->calculate_hand_value() < 21) {
 while ($dealer->calculate_hand_value() < 17) {
     // IF hand less than 17 hit
     $dealer->draw_card($deck->deal_card());
+}
+
+// Display final hands
+echo "<br>Final Hands:<br>";
+echo "Dealer's Hand: ";
+$dealer->display_hand();
+echo "<br>Player's Hand: ";
+$player->display_hand();
+
+
+// Determine winner through total scores
+$player_total = $player->calculate_hand_value();
+$dealer_total = $dealer->calculate_hand_value();
+
+if ($player_total > 21) {
+    echo "Player busts! Dealer wins!";
+} elseif ($dealer_total > 21) {
+    echo "Dealer busts! Player wins!";
+} elseif ($player_total > $dealer_total) {
+    echo "Player wins with $player_total against dealer's $dealer_total!";
+} elseif ($dealer_total > $player_total) {
+    echo "Dealer wins with $dealer_total against player's $player_total!";
+} else {
+    echo "It's a tie at $player_total!";
 }
 ?>
